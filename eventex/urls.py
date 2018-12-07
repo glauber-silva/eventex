bb5b1cf936a0976e1a498e9ace229fd31cabfe6c
glauber silva <glauber.lucio.silva@gmail.com>
2018-12-07 00:40:01 -0200

    fix import

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import eventex.core.views
+import eventex.core.views 
 
 urlpatterns = [
     path('', eventex.core.views.home),
