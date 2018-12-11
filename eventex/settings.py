cb4ad17b43c9d7c65a2f31d1728140287d49e9fa
glauber silva <glauber.lucio.silva@gmail.com>
2018-12-11 01:05:59 -0200

    - fix secret key

--- eventex/settings.py
+++ eventex/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd1ys_e^ykpig2--d*%7g@9bllaoer)$8a95b#a16(y=hjz!3(&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
